8b8198a783b5e4b81adc33b589ef4e83faed8b05
swetasachan777 <swetasachan777@gmail.com>
2025-01-14 04:04:21 +0530

    delete and update route completed

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ async function main() {
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
 });
+const methodoverride=require("method-override");
+app.use(methodoverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-
+app.use(express.urlencoded({extended:true}));
 app.get("/",(req,res)=>{
     res.send("hi, i am root");
 });
@@ -39,3 +41,48 @@ app.get("/listings",async(req,res)=>{
   const alllist=await Listing.find({});
   res.render("listings/index.ejs",{alllist});
   });
+
+     //new route
+app.get("/listings/new",async(req,res)=>{
+ 
+  res.render("listings/new.ejs",);
+  });
+
+  //show route
+  
+app.get("/listings/:id",async(req,res)=>{
+  let {id}=req.params;
+  const listing=await Listing.findById(id);
+  res.render("listings/show.ejs",{listing});
+  });
+
+  //create route
+  app.post("/listings",async(req,res)=>{
+    // let {title,description,price,image,country,location}=req.body;
+     let listing=new Listing(req.body.listing);
+    await listing.save();
+    res.redirect("/listings");
+  })
+
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+  let{id}=req.params;
+  const listing=await Listing.findById(id);
+  res.render("listings/edit.ejs",{listing});
+  
+})
+app.put("/listings/:id",async(req,res)=>{
+  let{id}=req.params;
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+ res.redirect("/listings");
+  
+})
+
+app.delete("/listings/:id",async(req,res)=>{
+  let{id}=req.params;
+  await Listing.findByIdAndDelete(id,{...req.body.listing});
+ res.redirect("/listings");
+  
+})
+
+
